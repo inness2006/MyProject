a0ff1f2c39d34d868da3a22323067e83c37d21f2
sebas <sebas@LAPTOP-Q4CV9BN2>
2024-01-28 21:10:49 +0000

    Add project files.

---
+++ MyProject/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace MyProject;
+
+public class Book
+{
+    private string title;
+    private string author;
+    private int yearPublished;
+    private bool isAvailable;
+
+    
+    public Book(string title, string author, int yearPublished)
+    {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.isAvailable = true;
+    }
+
+    
+    public void CheckOut()
+    {
+        if (isAvailable)
+        {
+            Console.WriteLine($"The book '{title}' by {author} has been checked out.");
+            isAvailable = false;
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, the book '{title}' is currently not available.");
+        }
+    }
+
+    
+    public void Return()
+    {
+        if (!isAvailable)
+        {
+            Console.WriteLine($"The book '{title}' has been returned.");
+            isAvailable = true;
+        }
+        else
+        {
+            Console.WriteLine($"Error: This book '{title}' is already available in the library.");
+        }
+    }
+
+  
+    public bool IsAvailable
+    {
+        get { return isAvailable; }
+    }
+}
